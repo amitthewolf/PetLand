60087677a367c7f5b9f0f86156ce79f45ff39108
amitthewolf <54326888+amitthewolf@users.noreply.github.com>
2022-01-20 14:19:26 +0200

    Add files via upload

--- Network/AsynchronousClient.cs
+++ Network/AsynchronousClient.cs
@@ -11,197 +11,50 @@ using UnityEngine;
 
 public class AsynchronousClient : MonoBehaviour
 {
-    private static bool ManagedConnection;
-    public Coroutine coroutine;
     public TMP_Text testText;
+    public State Indicator;
+    private static bool ManagedConnection;
     private Thread ClientConnection;
-    private char[] Seperators = new char[1];
     private List<string> LeaderboardList = null;
+    private string usernameToCheck;
+    private string PlayerUsername;
+    private string PlayerScore;
     private void Awake()
     {
-        Seperators[0] = ',';
         ManagedConnection = false;
-        GetLeaderboards();
     }
 
-    public void GetLeaderboards()
-    {
-        coroutine = StartCoroutine("TryGetLeaderboard");
-    }
-    
-    IEnumerator TryGetLeaderboard()
+    #region Add new Username
+
+    public void AddUserStart()
     {
         ManagedConnection = false;
-        LeaderboardList = null;
+        PlayerUsername = PlayerPrefs.GetString("Username");
+        StartCoroutine("TryAddUser");
+    }
+    
+    IEnumerator TryAddUser()
+    {
         while (!ManagedConnection)
         {
-            ClientConnection = new Thread(GetLeaderboard);
+            ClientConnection = new Thread(AddUserProcess);
             ClientConnection.Start();
             yield return new WaitForSeconds(5f);
         }
-        while (LeaderboardList == null)
-        {
-            yield return new WaitForSeconds(5f);
-        }
-        yield return LeaderboardList;
-    }
-    
-   private void GetLeaderboard()
-    {
-        byte[] bytes = new byte[1024];
-        try
-        {
-            // Connect to a Remote server
-            // Get Host IP Address that is used to establish a connection
-            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-            // If a host has multiple addresses, you will get a list of addresses
-            //IPHostEntry host = Dns.GetHostEntry("93.172.240.232");
-            IPAddress ipAddress = IPAddress.Parse("10.100.102.6");
-            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("10.100.102.6"), 11000);
-
-            // Create a TCP/IP  socket.
-            Socket sender = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Connect the socket to the remote endpoint. Catch any errors.
-            try
-            {
-                // Connect to Remote EndPoint
-                sender.Connect(remoteEP);
-                print("Socket connected to "+sender.RemoteEndPoint.ToString());
-                ManagedConnection = true;
-                // Encode the data string into a byte array.
-                byte[] msg = Encoding.ASCII.GetBytes("Leaderboard<EOF>");
-
-                // Send the data through the socket.
-                int bytesSent = sender.Send(msg);
-            
-                // Receive the response from the remote device.
-                string data = null;
-                while (true)
-                {
-                    bytes = new byte[1024];
-                    int bytesRec = sender.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    if (data.IndexOf("<EOF>") > -1)
-                    {
-                        break;
-                    }
-                }
-                print("Text received :"+ data);
-                testText.text = data;
-                List<string> UsersAndScores = data.Split(',').ToList();
-                // Release the socket.
-                // Encode the data string into a byte array.
-                msg = Encoding.ASCII.GetBytes("Bye have a great time<EOF>");
-
-                // Send the data through the socket.
-                sender.Send(msg);
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
-                LeaderboardList = UsersAndScores;
-                int counter = 0;
-                LeaderboardList.ForEach(x =>
-                {
-                    print(counter +" -> "+x);
-                    counter++;
-                });
-            }
-            catch (ArgumentNullException ane)
-            {
-                print("ArgumentNullException : "+ ane.ToString());
-                testText.text = "ArgumentNullException : "+ ane.ToString();
-            }
-            catch (SocketException se)
-            {
-                print("SocketException : "+ se.ToString());
-                testText.text = "SocketException : "+ se.ToString();
-            }
-            catch (Exception e)
-            {
-                print("Unexpected exception : "+ e.ToString());
-                testText.text = "Unexpected exception  : "+ e.ToString();
-            }
-        }
-        catch (Exception e)
-        {
-            testText.text = "Unexpected exception  : "+ e.ToString();
-        }
     }
    
-   public void updateScore()
-   {
-       coroutine = StartCoroutine("TryPostScore");
-   }
-    
-   IEnumerator TryPostScore()
-   {
-       ManagedConnection = false;
-       LeaderboardList = null;
-       while (!ManagedConnection)
-       {
-           ClientConnection = new Thread(PostScore);
-           ClientConnection.Start();
-           yield return new WaitForSeconds(5f);
-       }
-       while (LeaderboardList == null)
-       {
-           yield return new WaitForSeconds(5f);
-       }
-       yield return LeaderboardList;
-   }
-   
-   private void PostScore()
+    private void AddUserProcess()
     {
-        byte[] bytes = new byte[1024];
         try
         {
-            // Connect to a Remote server
-            // Get Host IP Address that is used to establish a connection
-            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-            // If a host has multiple addresses, you will get a list of addresses
-            IPHostEntry host = Dns.GetHostEntry("93.172.240.232");
-            IPAddress ipAddress = host.AddressList[0];
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
-
-            // Create a TCP/IP  socket.
-            Socket sender = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Connect the socket to the remote endpoint. Catch any errors.
+            Socket Connection = GetConnectionToServer();
             try
             {
-                // Connect to Remote EndPoint
-                sender.Connect(remoteEP);
-                print("Socket connected to "+sender.RemoteEndPoint.ToString());
-                ManagedConnection = true;
-                // Encode the data string into a byte array.
-                byte[] msg = Encoding.ASCII.GetBytes("Update - "+PlayerPrefs.GetString("Username") +" , "+ Player.GetScore() + "<EOF>");
-
                 // Send the data through the socket.
-                int bytesSent = sender.Send(msg);
-            
-                // Receive the response from the remote device.
-                string data = null;
-                while (true)
-                {
-                    bytes = new byte[1024];
-                    int bytesRec = sender.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    if (data.IndexOf("<EOF>") > -1)
-                    {
-                        break;
-                    }
-                }
-                print("Text received :"+ data);
+                SendMessage(Connection, "@@Insert@@"+PlayerUsername);
                 // Release the socket.
-                // Encode the data string into a byte array.
-                msg = Encoding.ASCII.GetBytes("Bye have a great time<EOF>");
-
-                // Send the data through the socket.
-                sender.Send(msg);
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
+                Connection.Shutdown(SocketShutdown.Both);
+                Connection.Close();
             }
             catch (ArgumentNullException ane)
             {
@@ -221,4 +74,275 @@ public class AsynchronousClient : MonoBehaviour
             print(e.ToString());
         }
     }
+
+    #endregion
+    
+    #region Get Leaderboards 
+
+    public void GetLeaderboardStart()
+    {
+        ManagedConnection = false;
+        StartCoroutine("TryGetLeaderboard");
+    }
+    
+    IEnumerator TryGetLeaderboard()
+    {
+        LeaderboardList = null;
+        while (!ManagedConnection)
+        {
+            ClientConnection = new Thread(GetLeaderboardProcess);
+            ClientConnection.Start();
+            yield return new WaitForSeconds(5f);
+        }
+    }
+    
+    private void GetLeaderboardProcess()
+    {
+        try
+        {
+            Socket Connection = GetConnectionToServer();
+            try
+            {
+                // Send the data through the socket.
+                SendMessage(Connection, "@@Leaderboard@@"+usernameToCheck);
+                // Receive the response from the remote device.
+                string data = ReceiveMessage(Connection);
+                print("Leaderboards Received");
+                List<string> UsersAndScores = data.Split('\n').ToList();
+                UsersAndScores.Remove("{");
+                UsersAndScores.Remove("}");
+                // Release the socket.
+                Connection.Shutdown(SocketShutdown.Both);
+                Connection.Close();
+                LeaderboardList = UsersAndScores;
+            }
+            catch (ArgumentNullException ane)
+            {
+                print("ArgumentNullException : "+ ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                print("SocketException : "+ se.ToString());
+            }
+            catch (Exception e)
+            {
+                print("Unexpected exception : "+ e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            print(e.ToString());
+        }
+    }
+   
+    public bool CheckLeaderboardList()
+    {
+        return LeaderboardList != null;
+    }
+    public List<string> GetLeaderboardList()
+    {
+        return LeaderboardList;
+    }
+
+    #endregion
+
+    #region Update Score
+
+    public void PostScoreStart()
+    {
+        ManagedConnection = false;
+        PlayerUsername = PlayerPrefs.GetString("Username");
+        PlayerScore = Player.GetScore().ToString();
+        StartCoroutine("TryPostScore");
+    }
+    
+    IEnumerator TryPostScore()
+    {
+        while (!ManagedConnection)
+        {
+            ClientConnection = new Thread(PostScoreProcess);
+            ClientConnection.Start();
+            yield return new WaitForSeconds(5f);
+        }
+    }
+   
+    private void PostScoreProcess()
+    {
+        try
+        {
+            Socket Connection = GetConnectionToServer();
+            try
+            {
+                // Send the data through the socket.
+                SendMessage(Connection, "@@Update@@"+PlayerUsername+":"+PlayerScore);
+                // Release the socket.
+                Connection.Shutdown(SocketShutdown.Both);
+                Connection.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                print("ArgumentNullException : "+ ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                print("SocketException : "+ se.ToString());
+            }
+            catch (Exception e)
+            {
+                print("Unexpected exception : "+ e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            print(e.ToString());
+        }
+    }
+
+    #endregion
+   
+    #region Username Availability Check
+   public void CheckUsernameStart(string UsernameToCheck)
+   {
+       Indicator = State.Waiting;
+       usernameToCheck = UsernameToCheck;
+       ManagedConnection = false;
+       StartCoroutine("TryCheckUsername");
+   }
+    
+   IEnumerator TryCheckUsername()
+   {
+       while (!ManagedConnection)
+       {
+           ClientConnection = new Thread(CheckUsernameProcess);
+           ClientConnection.Start();
+           yield return new WaitForSeconds(5f);
+       }
+   }
+   
+   private void CheckUsernameProcess()
+   {
+       try
+       {
+           Socket Connection = GetConnectionToServer();
+           try
+           {
+               // Send the data through the socket.
+               SendMessage(Connection, "@@CheckUsername@@"+usernameToCheck);
+               // Receive the response from the remote device.
+               string data = ReceiveMessage(Connection);
+               print("Text received :"+ data);
+               if (data.Contains("True"))
+                   Indicator = State.True;
+               else
+                   Indicator = State.False;
+               // Release the socket.
+               Connection.Shutdown(SocketShutdown.Both);
+               Connection.Close();
+           }
+           catch (ArgumentNullException ane)
+           {
+               print("ArgumentNullException : "+ ane.ToString());
+           }
+           catch (SocketException se)
+           {
+               print("SocketException : "+ se.ToString());
+           }
+           catch (Exception e)
+           {
+               print("Unexpected exception : "+ e.ToString());
+           }
+       }
+       catch (Exception e)
+       {
+           print(e.ToString());
+       }
+   }
+   
+
+   #endregion
+
+    #region Basic Functions
+
+    private Socket GetConnectionToServer()
+   {
+      try
+        {
+            // Connect to a Remote server
+            // Get Host IP Address that is used to establish a connection
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+            // If a host has multiple addresses, you will get a list of addresses
+            //IPAddress ipAddress = IPAddress.Parse("10.100.102.6");
+            IPHostEntry host = Dns.GetHostEntry("ec2-54-196-252-133.compute-1.amazonaws.com");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 2020);
+            // Create a TCP/IP  socket.
+            Socket sender = new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                // Connect to Remote EndPoint
+                sender.Connect(remoteEP);
+                print("Socket connected to "+sender.RemoteEndPoint.ToString());
+                ManagedConnection = true;
+                return sender;
+            }
+            catch (ArgumentNullException ane)
+            {
+                print("ArgumentNullException : "+ ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                print("SocketException : "+ se.ToString());
+            }
+            catch (Exception e)
+            {
+                print("Unexpected exception : "+ e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            print(e.ToString());
+        }
+      return null;
+   }
+   
+   private void SendMessage(Socket handler, string Message)
+   {
+       print("Sending out - "+Message);
+       byte[] msg = Encoding.ASCII.GetBytes(Message+"<EOF>");
+       handler.Send(msg);
+   }
+        
+   private string ReceiveMessage(Socket handler)
+   {
+       byte[] bytes = null;
+       string data = "";
+       while (true)
+       {
+           bytes = new byte[1024];
+           int bytesRec = handler.Receive(bytes);
+           data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+           if (data.IndexOf("<EOF>") > -1)
+           {
+               break;
+           }
+       }
+
+       return data;
+   }
+
+   #endregion
+  
+    
+   public bool CheckState()
+   {
+       return Indicator != State.Waiting;
+   }
+
+   public enum State
+   {
+       Waiting,
+       True,
+       False
+   }
 }  
